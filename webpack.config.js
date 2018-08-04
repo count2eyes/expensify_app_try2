d6d96e77db1a7cf14b98db5415e0988b93f65be3
j-dawg22 <jahangir2222@gmail.com>
2018-08-04 10:43:31 +0600

    first push to heroku server.js, start server script n heroku post-build

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,48 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/app.js",
-  module: {
-    rules: [
-      {
-        use: ["babel-loader"],
-        exclude: /node_modules/,
-        test: /\.js$/
-      },
-      {
-        use: ["style-loader", "css-loader", "sass-loader"],
-        test: /\.s?css$/
-      }
-    ]
-  },
-  output: {
-    filename: "bundle.js",
+module.exports = (env, argv) => {
+  const CSSExtract = new MiniCssExtractPlugin({ filename: "styles.css" });
+  return {
+    mode: "development",
+    entry: "./src/app.js",
+    module: {
+      rules: [
+        {
+          use: ["babel-loader"],
+          exclude: /node_modules/,
+          test: /\.js$/
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [CSSExtract],
+    output: {
+      filename: "bundle.js",
 
-    path: path.resolve(__dirname, "public")
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true
-  },
-  devtool: "cheap-module-eval-source-map"
+      path: path.resolve(__dirname, "public")
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true
+    },
+    devtool: "inline-source-map"
+  };
 };
